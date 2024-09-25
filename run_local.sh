c249b46774d78e044458602aa423817f592b215e
Giulio De Luise <giuliodeluise+github@gmail.com>
2024-09-25 16:36:24 +0200

    feat: run locally with vercel cli.

--- run_local.sh
+++ run_local.sh
@@ -1,8 +1,14 @@
-# Export secrets from .secrets
-while read -r line || [[ -n $line ]]; do    
-    [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
-    export $line
-done < .secrets
+if [ "$1" == "vercel" ]; then
+    # Run the web app on Vercel
+    vercel link --yes
+    vercel dev
+else
+    # Export secrets from .secrets
+    while read -r line || [[ -n $line ]]; do    
+        [[ "$line" =~ ^#.*$ || -z "$line" ]] && continue
+        export $line
+    done < .secrets
+    # Run the web app locally
+    uvicorn web:app
+fi
 
-# Run the web app on localhost
-uvicorn web:app
