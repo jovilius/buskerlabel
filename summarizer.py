8262d0e0b64c791510f1df3623926066c110bde7
Giulio De Luise <giuliodeluise+github@gmail.com>
2024-09-26 10:19:39 +0200

    fix: openai use in crawler.

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,10 @@
 import os
 import json
-import openai
+from openai import OpenAI
 
-openai.api_key = os.getenv("OPENAI_API_KEY").strip("\"")
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY").strip("\""),
+)
 
 def filter_and_summarize_ai_music(story: str) -> json:
     prompt = f"""
@@ -18,16 +20,14 @@ Story: '''{story}'''
 
 Return a JSON with the fields "check" and "summary".
 """
-
-    response = openai.ChatCompletion.create(
-        model="gpt-4o-mini",
+    response = client.chat.completions.create(
         messages=[
-            {"role": "system", "content": prompt}
+            {
+                "role": "user",
+                "content": prompt,
+            }
         ],
-        max_tokens=500,
-        n=1,
-        temperature=0,
+        model="gpt-4o-mini",
     )
-
-    reply = response.choices[0].message['content'].strip().strip("```").strip("json").strip()
+    reply = response.choices[0].message.content.strip().strip("```").strip("json").strip()
     return json.loads(reply)
